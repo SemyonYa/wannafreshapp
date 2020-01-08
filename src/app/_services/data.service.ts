236a2d3b6c1a9ef4ac12e0e5ea5aa22e2818fe86
unknown <mygarbage86@mail.ru>
2020-01-08 21:58:28 +0300

    data service example

--- src/app/_services/data.service.ts
+++ src/app/_services/data.service.ts
@@ -1,16 +1,29 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { BehaviorSubject } from 'rxjs';
+import { Farmer } from '../_models/farmer';
+import { environment } from 'src/environments/environment';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
-  farmers$ = new BehaviorSubject<string[]>([]);
+  farmers$ = new BehaviorSubject<Farmer[]>([]);
 
   constructor(private http: HttpClient) { }
 
   getFarmers() {
-    
+    this.http.get(environment.host + '/data/farmers')
+      .pipe(
+        map(
+          (data: any[]) => data.map(f => new Farmer(f.id, f.name, f.description, f.email, f.min_costs, f.delivery, f.img))
+        )
+      )
+      .subscribe(
+        (data: Farmer[]) => {
+          this.farmers$.next(data);
+        }
+      );
   }
 }
